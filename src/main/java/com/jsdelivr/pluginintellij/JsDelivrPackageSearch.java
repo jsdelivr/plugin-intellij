96e3b4189a562f653b66d7418808142cf81b7cb8
Martin Kolárik <martin@kolarik.sk>
2024-04-05 02:42:06 +0200

    Add getActionUpdateThread()

--- src/main/java/com/jsdelivr/pluginintellij/JsDelivrPackageSearch.java
+++ src/main/java/com/jsdelivr/pluginintellij/JsDelivrPackageSearch.java
@@ -1,11 +1,13 @@
 package com.jsdelivr.pluginintellij;
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.jsdelivr.pluginintellij.packagename.NameInput;
+import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
 
@@ -27,5 +29,10 @@ public class JsDelivrPackageSearch extends AnAction {
 
 		Font editorFont = editor.getContentComponent().getFont();
 		new NameInput(editor, project).setFont(new Font(editorFont.getFontName(), Font.PLAIN, editorFont.getSize() + 1));
+	}
+
+	@Override
+	public @NotNull ActionUpdateThread getActionUpdateThread() {
+		return ActionUpdateThread.EDT;
 	}
 }
--- src/main/java/com/jsdelivr/pluginintellij/actions/OpenGithub.java
+++ src/main/java/com/jsdelivr/pluginintellij/actions/OpenGithub.java
@@ -1,11 +1,18 @@
 package com.jsdelivr.pluginintellij.actions;
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
 
 public class OpenGithub extends AnAction {
 	@Override
 	public void actionPerformed(AnActionEvent event) {
 
+	}
+
+	@Override
+	public @NotNull ActionUpdateThread getActionUpdateThread() {
+		return ActionUpdateThread.EDT;
 	}
 }
